c635f8295102c400549e610babd8a64eb99a152f
vitaliy-kro <krochakv6@gmail.com>
2023-10-14 16:28:20 +0300

    feat: add delete modal and message after successfully delete

--- components/forms/DeleteThread.tsx
+++ components/forms/DeleteThread.tsx
@@ -5,6 +5,20 @@ import { usePathname, useRouter } from 'next/navigation';
 
 import { deleteThread } from '@/lib/actions/thread.actions';
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { useToast } from '@/components/ui/use-toast';
+import { ToastAction } from '@/components/ui/toast';
+
 interface Props {
   threadId: string;
   currentUserId: string;
@@ -22,23 +36,66 @@ function DeleteThread({
 }: Props) {
   const pathname = usePathname();
   const router = useRouter();
+  const { toast } = useToast();
 
   if (currentUserId !== authorId || pathname === '/') return null;
 
+  const handleDelete = async () => {
+    try {
+      await deleteThread(JSON.parse(threadId), pathname);
+
+      if (!parentId || !isComment) {
+        router.push('/');
+      }
+      toast({
+        title: 'Deleted',
+        description: 'Your thread successfully deleted',
+      });
+    } catch (e) {
+      toast({
+        variant: 'destructive',
+        title: 'Uh oh! Something went wrong.',
+        action: (
+          <ToastAction
+            altText="Try again"
+            onClick={async () => {
+              await deleteThread(JSON.parse(threadId), pathname);
+            }}
+          >
+            Try again
+          </ToastAction>
+        ),
+      });
+    }
+  };
+
   return (
-    <Image
-      src="/assets/delete.svg"
-      alt="delte"
-      width={18}
-      height={18}
-      className="cursor-pointer object-contain"
-      onClick={async () => {
-        await deleteThread(JSON.parse(threadId), pathname);
-        if (!parentId || !isComment) {
-          router.push('/');
-        }
-      }}
-    />
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Image
+          src="/assets/delete.svg"
+          alt="delete"
+          width={18}
+          height={18}
+          className="cursor-pointer object-contain"
+        />
+      </AlertDialogTrigger>
+      <AlertDialogContent className="bg-dark-1">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription className="text-light-1">
+            This action cannot be undone. This will permanently delete your post
+            and remove it from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDelete} className="bg-primary-500">
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
 
